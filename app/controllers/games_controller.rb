49bf51b436284f7b00630c4c7f3264fd27b4ae18
Laura Auroux <laura.auroux@live.fr>
2021-05-10 17:18:15 +0200

    longest word done

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,7 +1,21 @@
+require 'open-uri'
+require 'json'
+
 class GamesController < ApplicationController
   def new
+    alphabet = ("A".."Z").to_a
+    @letters = 10.times.map { alphabet.sample }
   end
 
   def score
+    @word = params[:word]
+    @grid = params[:token].split('')
+    @array_of_letters = @word.split('')
+
+    @url = "https://wagon-dictionary.herokuapp.com/#{@word}"
+    @serialized_word = URI.open(@url).read
+    @nice_word = JSON.parse(@serialized_word)
+    
+    @can_be_built = @array_of_letters.all? { |letter| @array_of_letters.count(letter) <= @grid.count(letter) }
   end
 end
